a3644e2c63cf27b2569da2a9bc7e4b48048d8030
Piotr Senkow <piotrsenkow@gmail.com>
2025-08-26 15:31:04 -0500

    Fixing test

--- tests/test_drsclient.py
+++ tests/test_drsclient.py
@@ -291,7 +291,7 @@ def test_access_endpoint(index_client, drs_client, async_client):
     rec = create_index_record(index_client)
     did = rec["did"]
     protocol = "s3"
-    full_url = drs_client.url + "/ga4gh/drs/v1/objects/" + did + "/access/" + protocol
+    full_url = drs_client.url_for("/ga4gh/drs/v1/objects", did, "access", protocol)
     request = respx.get(full_url).mock(return_value=httpx.Response(200))
     loop = asyncio.get_event_loop()
 
