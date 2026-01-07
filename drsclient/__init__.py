0df9ef8db00d4876056846b37aad340b25f4b4be
Nina <randorf@uchicago.edu>
2026-01-07 15:04:31 -0600

    dependency updates, deprecation warning

--- drsclient/__init__.py
+++ drsclient/__init__.py
@@ -1,3 +1,3 @@
-import pkg_resources
+# import pkg_resources
 
-__version__ = pkg_resources.get_distribution("drsclient").version
+# __version__ = pkg_resources.get_distribution("drsclient").version
